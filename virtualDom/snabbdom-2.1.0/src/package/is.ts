9c143ec07395d573f9ef7316a7df88de030b9f9e
lilangxiong <apple@appledeMacBook-Pro.local>
2021-03-10 20:23:18 +0800

    完善面试题目

--- virtualDom/snabbdom-2.1.0/src/package/is.ts
+++ virtualDom/snabbdom-2.1.0/src/package/is.ts
@@ -2,14 +2,10 @@
  * @Author: LiLangXiong680
  * @Date: 2020-09-13 22:09:56
  * @LastEditors: LiLangXiong680
- * @LastEditTime: 2021-03-09 21:04:48
+ * @LastEditTime: 2021-03-10 17:39:13
  * @FilePath: /plan/virtualDom/snabbdom-2.1.0/src/package/is.ts
  */
 export const array = Array.isArray
 export function primitive(s: any): s is (string | number) {
   return typeof s === 'string' || typeof s === 'number'
 }
-
-```
-
-## patch
